a340d8700fed2bf95b40d5e90325e655cd88841c
Mariah Jariwala <mj1415@g.rit.edu>
2024-11-23 22:42:55 +0400

    initial commit

--- polygon.py
+++ polygon.py
@@ -1,68 +1,40 @@
 class Polygon:
-    """
-    A class for polygons with attributes for a name, sides, and methods for
-    calculations and comparisons.
-    """
-    def __init__ (self, name, sides):
+    def __init__(self, name: str, sides: list[float]):
+
         self.name = name
         self.sides = sides
 
-# Getter and setter for name
     def get_name(self):
         return self.name
-    def set_name(self, name):
-        self.name = name
-    
-#Getter and setter for sides
+
     def get_sides(self):
         return self.sides
-    def set_sides(self, sides):
-        self.sides = sides
+    
 
-# Implements Equality and Inequality Methods
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.name == other.name and self.sides == other.sides
-    
-    def __ne__(self, other):
-        return not self.__eq__(other)
-    
-# string representation for the class
-    def __str__(self):
-        return f"{self.name} with sides: {self.sides}"
-    
-# calculates the circumference for the polygon
-    def calculate_circumference(self):
-        sum = 0
-        for side in self.sides:
-            sum += side
-        return f"The circumference is {sum}"
-    
+
+
+
+
+
+
+
+
 def main():
     """
-    This function is the main function that 
-    demonstrates the use of the class and takes no parameters
+    Creates and demonstrates Polygon objects.
     """
 
-    #objects
-    triangle = Polygon("Triangle", [3, 3, 4])
-    square = Polygon("Square", [2, 2, 2, 2])
-    hexagon = Polygon("Hexagon", [2, 2, 2, 2, 2, 2])
-    
-    print(triangle)
-    print(triangle.calculate_circumference())
+    # Create polygon objects
+    triangle = Polygon("Triangle", [3, 4, 5])
+    square = Polygon("Square", [4, 4, 4, 4])
+    hexagon = Polygon("Hexagon", [6, 6, 6, 6, 6, 6])
 
-    print(square)
-    print(square.calculate_circumference())
+    # Print object information and circumference
+    for polygon in [triangle, square, hexagon]:
+        print(f"{polygon}")
+        circumference = polygon.calculate_circumference()
+        print(f"  Circumference: {circumference:.2f}")
 
-    print(hexagon)
-    print(hexagon.calculate_circumference())
-    
+
 if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
+  main()
